304f4647b15e54e7f97023f29d93a29548bc5b44
Ajala John <150349868+Johnnit@users.noreply.github.com>
2024-03-13 11:47:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let ham = document.getElementById("bars");
-let navLinks = document.querySelector(".nav-links");
-ham.addEventListener("click",()=>{
-  alert('help me');
-  navLinks.classList.toggle("show");
-})
